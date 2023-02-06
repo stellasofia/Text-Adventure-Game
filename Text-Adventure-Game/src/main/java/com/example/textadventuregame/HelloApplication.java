9243731bfb2e66cc43b4a38888b8c0cf8471ca70
stellasofia <stellasofiagross@gmail.com>
2023-02-06 22:50:29 +0100

    v.1

--- Text-Adventure-Game/src/main/java/com/example/textadventuregame/HelloApplication.java
+++ Text-Adventure-Game/src/main/java/com/example/textadventuregame/HelloApplication.java
@@ -2,7 +2,12 @@ package com.example.textadventuregame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +15,40 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Group root = new Group();
+        Scene scene = new Scene(root,600,600, Color.BLACK);//add root to Scene
+        stage.setResizable(false);
+
+        stage.setTitle("Text Adventure Game");
+
+        Text text = new Text(); //type of node needs to be added to a root node
+        text.setText("ADVENTURE");
+        //position - starting and ending coordinates
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Veranda", 50));
+        text.setFill(Color.WHITE);
+
+        Line line = new Line(); //its a node
+        line.setStartX(0);
+        line.setStartY(400);
+        line.setEndX(600);
+        line.setEndY(400);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.WHITE);
+
+
+
+        root.getChildren().add(text); //to display text
+        root.getChildren().add(line); // to display line
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+
     }
 }
